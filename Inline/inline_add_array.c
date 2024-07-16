a0c9091166239ba9ac6ad5ab7da69b0c59820e77
Pooja Ransing <147318127+PoojaSing24@users.noreply.github.com>
2024-07-16 08:05:34 +0530

    Add files via upload

---
+++ Inline/inline_add_array.c
@@ -0,0 +1,15 @@
+//write a program to sum of the given array--------
+//
+#include<stdio.h>
+
+int n=5;
+int Array[5]={1,2,3,4,5};
+int sum;
+
+int main()
+{
+
+
+
+printf("addition is: %d",sum);
+}
