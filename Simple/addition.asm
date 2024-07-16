9095c66542751c4300f5bb5e27d09ca1d3a929a7
Pooja Ransing <147318127+PoojaSing24@users.noreply.github.com>
2024-07-16 07:42:49 +0530

    Create addition.asm

---
+++ Simple/addition.asm
@@ -0,0 +1,73 @@
+;add two given numbers---------------
+
+
+;section .data
+;	num1 dd 0104
+;	num2 dd 0305
+;	sum db"addition of two number is: %d",10,0
+
+;
+;section .text
+;	global main
+;	extern printf
+	
+;main:
+;	mov eax,[num1];load the values at address num1 into eax
+;	add eax,[num2]
+;	
+;	push eax
+;	push sum
+;	call printf
+;	add esp,8
+;	ret
+
+
+;---------------------------------------------------------------------------
+
+;print addition of given any two no-----
+
+section .data
+	msg1 db"value of n1 :",10,0
+	num1 db"%d",0
+	msg2 db"value of n2 :",10,0
+	sum db"addition is: %d",10,0
+
+section .bss
+	n1 resd 1
+	n2 resd 1
+
+section .text
+	global main
+	extern printf,scanf
+
+	
+main:
+	push msg1
+	call printf
+	add esp,4
+
+	push n1
+	push num1
+	call scanf
+	add esp,8
+	
+	push msg2
+	call printf
+	add esp,4
+
+	push n2
+	push num1
+	call scanf
+	add esp,8
+	
+	mov eax,[n1]
+	add eax,[n2]
+
+	push eax
+	push sum
+	call printf
+	add esp,8
+
+	ret
+	
+
